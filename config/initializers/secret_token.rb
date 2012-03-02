bd9da8988532ee223df8773ef28e6cd00cff8d79
Simon Podhajsky <simon.podhajsky@gmail.com>
2012-03-02 16:28:50 -0500

    Rails Install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paradiso::Application.config.secret_token = 'd8485ade190344563032c1e4496d4a44034f06a620a5e7f2abdee1907bb534b6a7c0352d3c055251eedf7b929e120e1ed9e6d29bf24e9cc74027f589cb9ce973'
