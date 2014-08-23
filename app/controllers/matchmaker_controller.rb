8c94dce2c4bd85e3c1787d8091e302d3c12fce6f
Jiaao Hou <jiaao.hou@yale.edu>
2014-08-23 15:39:40 -0400

    save record and request in swap

--- app/controllers/matchmaker_controller.rb
+++ app/controllers/matchmaker_controller.rb
@@ -1,7 +1,7 @@
 class MatchmakerController < ApplicationController
 	def self.swap
 		requests = Request.where(active: true)
-		suggested = findBestSolution(requests) #returns list of request IDs
+		suggested = [1, 2, 3, 4, 5] #findBestSolution(requests) #returns list of request IDs
 
 		suggested.each do |request_id|
 			request = Request.find(request_id)
@@ -9,9 +9,11 @@ class MatchmakerController < ApplicationController
 			student = request.student
 			section = request.original_section
 			record = Attendance.where(student: student, section: section).first
+
 			record.update_attributes(section: request.target_section)
 			# TODO: transaction each closed graph / the entire solution
 			request.active = false
+			request.save
 		end
 	end
 end
