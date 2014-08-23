3db385abec8b9f2b38427f8defe6fed4e4e07b97
squidgetx <squidgetx@gmail.com>
2014-08-23 15:57:23 -0400

    request indexing

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,10 @@
 class RequestsController < ApplicationController
 
+  def index
+    @pending_requests = Request.where('active = true')
+    @fulfilled_requests = Request.where('active = false')
+  end
+
   def new
     @request = Request.new
     @sections = Section.all
