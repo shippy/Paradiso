0b9626e151251a72d22a379f869768c418ef90bc
nagn <mlin4589@gmail.com>
2014-08-23 12:10:49 -0400

    Added TargetValidator

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,7 +1,18 @@
+class TargetValidator < ActiveModel::Validator
+  def validate(record)
+    if record.original_section == record.target_section
+      record.errors[:target_section] << "Target Section needs to be different from the current Section." 
+    end
+  end
+end
+
 class Request < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates_with TargetValidator
   belongs_to :student
   belongs_to :original_section, class_name: 'Section'
   belongs_to :target_section, class_name: 'Section'
   
   validates :student, :original_section, :target_section, :active, presence: true
 end
+
