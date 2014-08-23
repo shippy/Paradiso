40f1decf8218a1153918229b90066b01004f5288
nagn <mlin4589@gmail.com>
2014-08-23 13:29:02 -0400

    Added custom validations to Request and Attendance to make sure that Students don't attempt to sign up for multiple sessions of the same class or request a change to the same class

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,13 @@
 class Attendance < ActiveRecord::Base
   belongs_to :student
   belongs_to :section
+  
+  validate :one_course_per_student
+  
+  def one_course_per_student
+    courses = student.sections.pluck(:course_id)
+    unless courses.length == courses.uniq.length
+      errors.add(:section, "Student cannot attend multiple sections of the same course.")
+    end
+  end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,18 +1,16 @@
-class TargetValidator < ActiveModel::Validator
-  def validate(record)
-    if record.original_section == record.target_section
-      record.errors[:target_section] << "Target Section needs to be different from the current Section." 
-    end
-  end
-end
-
 class Request < ActiveRecord::Base
-  include ActiveModel::Validations
-  validates_with TargetValidator
   belongs_to :student
   belongs_to :original_section, class_name: 'Section'
   belongs_to :target_section, class_name: 'Section'
   
   validates :student, :original_section, :target_section, :active, presence: true
+  validate :target_differs_from_origin
+  
+  def target_differs_from_origin
+    if original_section == target_section
+      errors.add(:target_section, "Target Section needs to be different from the current Section." )
+    end
+    
+  end
 end
 
