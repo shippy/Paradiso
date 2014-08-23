0e90c51b2fe026cce1648666c9adb3234127b28e
squidgetx <squidgetx@gmail.com>
2014-08-23 12:29:50 -0400

    scrollto!!!

--- app/assets/javascripts/new_request.js
+++ app/assets/javascripts/new_request.js
@@ -1,8 +1,27 @@
 $(document).ready(function() {
-  $('#next').click(function() {
-    alert('test');
+  slides = ["#welcome", "#otherdiv", "#thirddiv"];
 
+  $('#next').data('index',0);
+
+  $('#next').click(function() {
+    var target = parseInt($(this).data('index')) + 1;
+    scroll(slides[target]);
+    $(this).data('index', target);
+  });
+
+  $('#top').click(function() {
+    var target = parseInt($('#next').data('index') - 1);
+    if (target < 0) {
+      target = 0;
+    }
+    scroll(slides[target]);
+    $('#next').data('index',target);
   });
 
 
+
 });
+
+var scroll = function(selector) {
+  $.scrollTo(selector,800);
+}
